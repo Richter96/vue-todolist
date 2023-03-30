38be6bb60b325c18805db56ce798157c450b3689
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-03-30 17:36:57 +0200

    add barr text

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,17 @@
 /* Descrizione:
-Rifare l'esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-text, una stringa che indica il testo del todo
-done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+Rifare l'esercizio della to do list. Questa volta però ogni done sarà un oggetto, formato da due proprietà:
+text, una stringa che indica il testo del done
+done, un booleano (true/false) che indica se il done è stato fatto oppure no
 MILESTONE 1
-Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+Stampare all'interno di una lista HTML un item per ogni done. Se la proprietà done è uguale a true, visualizzare il testo del done sbarrato.
 
 
 
 
 MILESTONE 2
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il done viene rimosso dalla lista.
 MILESTONE 3
-Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. */
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo done, che quindi viene aggiunto alla lista dei done esistenti. */
 
 
 const { createApp } = Vue
@@ -23,20 +23,20 @@ createApp({
       newItemName: '',
       itemsStructure : {
         nome:'',
-        todo:false
+        done:false
       },
       items: [
         {
           nome: 'pasta',
-          todo: 'false'
+          done: 'false'
         },
         {
           nome: 'carote',
-          todo: 'false'
+          done: 'false'
         },
         {
           nome: 'uova',
-          todo: 'false'
+          done: 'false'
         }
       ],
     }
@@ -46,13 +46,13 @@ createApp({
       console.log('aggiungi nuovo elemento');
       console.log(this.newItemName);
       const newItem = {
-        nome: this.newItemName,
-        todo:false
+        nome:"",
+        done:false
       } 
       newItem.nome = this.newItemName
       console.log(newItem)
-
       this.items.push(newItem)
+      this.newItemName=""
     }
   },
   
